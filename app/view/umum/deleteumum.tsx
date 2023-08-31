d56bb8971b20c1773f2eec55d5354997892d99c0
hamam28 <119409563+hamam28@users.noreply.github.com>
2023-08-31 22:18:11 +0800

    Update deleteumum.tsx

--- app/view/umum/deleteumum.tsx
+++ app/view/umum/deleteumum.tsx
@@ -19,7 +19,7 @@ export default function DeletePegawai(linkumum: ItemUmum) {
     async function handleDelete() {
         setIsMutating(false);
         setModal(false);
-        const res = await fetch(`http://efeksi.vercel.app/api/pubs?id=${linkumum?._id}`,{
+        const res = await fetch(`http://efeksi-git-master-hamam28.vercel.app/api/pubs?id=${linkumum?._id}`,{
             method: "DELETE",
         });
         router.refresh();
